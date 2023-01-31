9789e96bca8e7f6bf46fd3039754780257479e35
Prof John David Zehnpfennig II, PE <111508888+prof-jz@users.noreply.github.com>
2023-01-31 22:07:28 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@L7FunctionExample/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+void boilPasta();
+void howToFish(int numFish);
+
+
+int main() 
+{
+  int numberFish = 0;
+  boilPasta();
+  
+  cout << "\nHow many fish do you want to catch?\n";
+  cin >> numberFish;
+
+  howToFish(numberFish);
+  cout << "\nThanks, thje program is over.";
+  
+  return 0;
+}
+
+//JZ - invoke our functions
+void boilPasta()
+{
+//code goes here
+  cout << "\nYay!  The pasta boiled nicely.  Al dete.\n";
+  
+}
+
+
+
+void howToFish(int numFish)
+{
+  //code goes here
+  cout << "\nYou will catch " << numFish << " fish.\n";
+  
+}
