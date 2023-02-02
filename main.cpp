183fd424e6462885ffc86b7224f7baa0c5e3ef1f
Prof John David Zehnpfennig II, PE <111508888+prof-jz@users.noreply.github.com>
2023-02-02 21:21:49 +0000

    Initial state for Lesson 8.  main() with two minimally viable functions.  L8 will add & test a menu in main() that calls the functions.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,11 @@
+/*0*******************************************
+* Alabama A&M University - EE109 - Spring 2023
+* Lesson 7 & 8 sample code: functions and 'if'
+*   & 'else if'  operations
+* By:            Prof J Zehnpfennig, PE
+* Created:       31 January 2023
+* Last Updated:  02 Februrary 2023
+*********************************************/
 #include <iostream>
 
 using namespace std;
@@ -5,34 +13,49 @@ using namespace std;
 void boilPasta();
 void howToFish(int numFish);
 
-
 int main() 
 {
   int numberFish = 0;
   boilPasta();
   
+  /*
+  Place menu here
+  */
+  
+  //collect user input
+
+  //launch selected function or exit
   cout << "\nHow many fish do you want to catch?\n";
   cin >> numberFish;
-
   howToFish(numberFish);
+  
+  //return to menu
+  
+  
   cout << "\nThanks, the program is over.";
   
   return 0;
 }
 
 //JZ - invoke our functions
-void boilPasta()
+//this is selection "1" from the menu
+void boilPasta()  //JZ - takes no values in and returns nothing to the calling function
 {
-//code goes here
-  cout << "\nYay!  The pasta boiled nicely.  Al dete.\n";
+  /*
+  Your code goes here
+  */
+  
+  cout << "\nYay!  The pasta boiled nicely.  Al dente.\n";  //JZ - example line to show the function worked.  You can delete it.
   
 }
 
-
-
-void howToFish(int numFish)
+//this is selection "2" from the menu
+void howToFish(int numFish)  //JZ takes in an integer valuse numFish and returns nothing to the calling function.
 {
-  //code goes here
-  cout << "\nYou will catch " << numFish << " fish.\n";
+  /*
+  Your code goes here
+  */
+  
+  cout << "\nYou will catch " << numFish << " fish.\n";     //JZ - example line to show the function worked.  You can delete it.
   
 }
